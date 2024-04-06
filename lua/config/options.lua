05d7ba3b7ff327cb4b5134ec2f2a5e7b311b99bf
vantyc <vantyc@hotmail.com>
2024-04-06 17:12:08 -0600

    nvim con lsp

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+-- vim.cmd([[ set background=dark ]])
+vim.cmd([[ set encoding=UTF-8 ]])
+vim.cmd([[ set mouse=a ]])
+vim.cmd([[ set mouse-=a ]])
+vim.wo.relativenumber=true
+vim.wo.number=true
+-- vim.wo.mouse=a
+-- vim.wo.enconding=UTF-8
+-- vim.wo.background=dark
---
+++ lua/plugins/one_monokai.lua
@@ -0,0 +1,6 @@
+return {
+	"cpea2506/one_monokai.nvim",
+	config = function()
+	require("one_monokai").setup()
+end,
+}
