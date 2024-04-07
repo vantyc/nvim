2f61f7ec16919bfde9644fb77f988801fcb97fb0
vantyc <vantyc@hotmail.com>
2024-04-07 09:58:47 -0600

    con pestañas

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,14 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
--- vim.cmd([[ set background=dark ]])
 vim.cmd([[ set encoding=UTF-8 ]])
 vim.cmd([[ set mouse=a ]])
 vim.cmd([[ set mouse-=a ]])
 vim.wo.relativenumber=true
 vim.wo.number=true
+
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtavsotp = 4
+vim.o.shiftwidth = 4
+
 -- vim.wo.mouse=a
 -- vim.wo.enconding=UTF-8
 -- vim.wo.background=dark
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,32 +1,29 @@
 return {
-{
-	"williamboman/mason.nvim",
-	config = function()
-		require("mason").setup()
-	end
-},
-{
-	"williamboman/mason-lspconfig.nvim",
-	config = function()
-		require("mason-lspconfig").setup({
-			ensure_installed={"lua_ls"}
-		})
-	end
-},
-{
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup({})
-		vim.keymap.set('n','K', vim.lsp.buf.hover, {})
-	end
-},
-{
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lspconfig = require("lspconfig")
-		lspconfig.jdtls.setup({})
-		vim.keymap.set('n','K', vim.lsp.buf.hover, {})
-	end
-}
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "jdtls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.jdtls.setup({})
+			lspconfig.lua_ls.setup({})
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
