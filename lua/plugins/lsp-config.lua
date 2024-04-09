55c28e951ff7db85b7676a78615059a26d10614e
vantyc <vantyc@hotmail.com>
2024-04-09 01:56:50 -0600

    configuracion con lombok trabajando

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -29,9 +29,10 @@ return {
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 			local lspconfig = require("lspconfig")
-			lspconfig.jdtls.setup({
-                capabilities = capabilities
-            })
+
+			--lspconfig.jdtls.setup({
+             --  capabilities = capabilities
+            --})
 			lspconfig.lua_ls.setup({
                 capabilities = capabilities
             })
