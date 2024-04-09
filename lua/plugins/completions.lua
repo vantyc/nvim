4588df6f890e94b8aa02444580cdddcc9a4324cc
vantyc <vantyc@hotmail.com>
2024-04-08 19:25:02 -0600

    lsp completion

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,5 +1,8 @@
 return {
-	{
+    {
+        "hrsh7th/cmp-nvim-lsp"
+    },
+ {
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
@@ -11,8 +14,7 @@ return {
 		config = function()
 			-- Set up nvim-cmp.
 			local cmp = require("cmp")
-            require("luasnip.loaders.from_vscode").lazy_load()
-            
+			require("luasnip.loaders.from_vscode").lazy_load()
 			cmp.setup({
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
@@ -29,13 +31,48 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					--					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					--					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					--					["<C-Space>"] = cmp.mapping.complete(),
+					--					["<C-e>"] = cmp.mapping.abort(),
+					--					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					--					     ['<C-e>'] = cmp.mapping.close(),
+					["<CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Insert,
+						select = true,
+					}),
+					["<Tab>"] = function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end,
+					["<S-Tab>"] = function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end,
+					["<C-j>"] = function(fallback)
+						if cmp.visible() then
+							cmp.scroll_docs(4)
+						else
+							fallback()
+						end
+					end,
+					["<C-k>"] = function(fallback)
+						if cmp.visible() then
+							cmp.scroll_docs(-4)
+						else
+							fallback()
+						end
+					end,
 					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
 				sources = cmp.config.sources({
+					{ name = "path" },
 					{ name = "nvim_lsp" },
 					--{ name = "vsnip" }, -- For vsnip users.
 					{ name = "luasnip" }, -- For luasnip users.
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,12 +1,21 @@
 return {
 	{
 		"williamboman/mason.nvim",
+        lazy = false,
 		config = function()
 			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
 		config = function()
 			require("mason-lspconfig").setup({
 				ensure_installed = { "lua_ls", "jdtls" },
@@ -15,10 +24,17 @@ return {
 	},
 	{
 		"neovim/nvim-lspconfig",
+        lazy = false,
 		config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.jdtls.setup({})
-			lspconfig.lua_ls.setup({})
+			lspconfig.jdtls.setup({
+                capabilities = capabilities
+            })
+			lspconfig.lua_ls.setup({
+                capabilities = capabilities
+            })
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
