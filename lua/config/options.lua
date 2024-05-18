051f5028ab217b7af94dfa0ffea4a81d9889eaa0
Rafael Carrasco <vantyc@hotmail.com>
2024-05-18 11:10:24 -0600

    clipboard definitivo

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,6 @@
 vim.cmd([[ set encoding=UTF-8 ]])
-vim.cmd([[ set mouse=a ]])
-vim.cmd([[ set mouse-=a ]])
+-- vim.cmd([[ set mouse=a ]])
+-- vim.cmd([[ set mouse-=a ]])
 vim.wo.relativenumber=true
 vim.wo.number=true
 
@@ -8,7 +8,4 @@ vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.softtavsotp = 4
 vim.o.shiftwidth = 4
-
--- vim.wo.mouse=a
--- vim.wo.enconding=UTF-8
--- vim.wo.background=dark
+vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
