f727364ca843f1dc225d1dfed85eefea57f0eb1c
Rafael Carrasco <vantyc@hotmail.com>
2024-06-26 09:10:58 -0600

    barbar

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,7 @@ vim.wo.number=true
 
 vim.o.tabstop = 4
 vim.o.expandtab = true
-vim.o.softtavsotp = 4
+--vim.o.softtavsotp = 4
+vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
